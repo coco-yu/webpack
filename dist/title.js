9fc09c2fba7b0ff3f31edff5b84f132e39d65fe3
coco-yu <359274291@qq.com>
2020-07-12 15:10:35 +0800

    webpack

---
+++ dist/title.js
@@ -0,0 +1,19 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["title"],{
+
+/***/ "./src/title.js":
+/*!**********************!*\
+  !*** ./src/title.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = {
+    name: 'title_name',
+    age: 'title_age'
+};
+
+
+
+/***/ })
+
+}]);
